83b40b09e8c9423d396d2cb03854a351999cda2a
Oso Oluwafemi <osofem87@gmail.com>
2017-06-12 16:11:02 +0100

    Add files via upload

--- assert.js
+++ assert.js
@@ -1,3 +1,10 @@
+function assertExpect(actual, expected, message){
+    if(expected != actual){
+        message = ((typeof message != "undefined")? message+" ":"") + "Assertion failed. Expected " + JSON.stringify(expected) + " instead got " + JSON.stringify(actual);
+        console.error(message);
+    }else console.log("%cTest passed. "+ JSON.stringify(actual) + " === " + JSON.stringify(expected), "color: green;");
+}
+
 function assertEqual(actual, expected, message){
     if(actual !== expected){
         message = ((typeof message != "undefined")? message+" ":"") + "Assertion failed. Expected "+((typeof expected == "string")?"\"":"")+ expected +((typeof expected == "string")?"\"":"")+ " instead got "+((typeof actual == "string")?"\"":"")+ actual +((typeof actual == "string")?"\"":"");
@@ -7,7 +14,15 @@ function assertEqual(actual, expected, message){
 
 function assertDeepEqual(actual, expected, message){
     if(JSON.stringify(actual) !== JSON.stringify(expected)){
-        message = ((typeof message != "undefined")? message+" ":"") + "Assertion failed. Expected " + ((typeof expected == "string")?"\"":"") + JSON.stringify(expected) + ((typeof expected == "string")?"\"":"") + " instead got " + ((typeof actual == "string")?"\"":"") + JSON.stringify(actual) +((typeof actual == "string")?"\"":"");
+        message = ((typeof message != "undefined")? message+" ":"") + "Assertion failed. Expected " + JSON.stringify(expected) + " instead got " + JSON.stringify(actual);
         console.error(message);
-    }else console.log("%cTest passed. "+((typeof actual == "string")?"\"":"")+ JSON.stringify(actual) +((typeof actual == "string")?"\"":"")+ " === " +((typeof expected == "string")?"\"":"")+ JSON.stringify(expected) +((typeof expected == "string")?"\"":""), "color: green;");
+    }else console.log("%cTest passed. "+ JSON.stringify(actual) + " === " + JSON.stringify(expected), "color: green;");
+}
+
+function assertIsNaN(actual){
+	if(!isNaN(actual)){
+		message = ((typeof message != "undefined")? message+" ":"") + "Assertion failed. Expected " + NaN + " instead got " + JSON.stringify(actual);
+        console.error(message);
+	}
+	else console.log("%cTest passed. "+ actual + " === " + NaN, "color: green;");
 }
